68fc149f67da3461dbd34e5e6f19a2ab2c5087bb
baldy777 <jhoelquispetapia315@gmail.com>
2025-10-05 23:09:03 -0400

    feat: adicion de Navbar y funciones de enrutamiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar, { SidebarItem } from "./components/Navbar";
+import "./App.css";
+import { Home, Users } from "lucide-react";
+
+import VistaUsuarios from "./pages/VistaUsuarios";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="flex">
+        {/* Barra lateral */}
+        <Navbar>
+          <SidebarItem icon={<Home />} text="Inicio" to="/" />
+          <SidebarItem icon={<Users />} text="Usuarios" to="/tabla-usuarios" />
+        </Navbar>
+
+        {/* Contenido principal */}
+        <main className="flex-1 ml-16 p-6">
+          <Routes>
+            <Route path="/" element={<h1 className="text-black">Página de Inicio</h1>} />
+            <Route path="/tabla-usuarios" element={<VistaUsuarios />} />
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
