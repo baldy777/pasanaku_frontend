98e6d1d640d5c45d26b0f008555df4a1e9fa9c30
baldy777 <jhoelquispetapia315@gmail.com>
2025-10-16 20:41:24 -0400

    feat: adicion de rutas principales login y registro

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,34 @@ import "./App.css";
 import { Home, Users } from "lucide-react";
 
 import VistaUsuarios from "./pages/usuarios/VistaUsuarios";
+import RegistroUsuarios from "./pages/usuarios/RegistroUsuarios";
+import InicioSesionUsuarios from "./pages/usuarios/login";
 
 function App() {
   return (
     <Router>
-      <div className="flex">
-        {/* Barra lateral */}
-        <Navbar>
-          <SidebarItem icon={<Home />} text="Inicio" to="/" />
-          <SidebarItem icon={<Users />} text="Usuarios" to="/tabla-usuarios" />
-        </Navbar>
+      <Routes>
+        {/* Ruta inicial - Login */}
+        <Route path="/" element={<InicioSesionUsuarios />} />
+        <Route path="/registro" element={<RegistroUsuarios />} />
 
-        {/* Contenido principal */}
-        <main className="flex-1 ml-16 p-6">
-          <Routes>
-            <Route path="/" element={<h1 className="text-black">Página de Inicio</h1>} />
-            <Route path="/tabla-usuarios" element={<VistaUsuarios />} />
-          </Routes>
-        </main>
-      </div>
+        {/* Rutas protegidas con sidebar */}
+        <Route path="/*" element={
+          <div className="flex">
+            <Navbar>
+              <SidebarItem icon={<Home />} text="Inicio" to="/inicio" />
+              <SidebarItem icon={<Users />} text="Usuarios" to="/tabla-usuarios" />
+            </Navbar>
+
+            <main className="flex-1 ml-16 p-6">
+              <Routes>
+                <Route path="/inicio" element={<h1 className="text-black">Página de Inicio</h1>} />
+                <Route path="/tabla-usuarios" element={<VistaUsuarios />} />
+              </Routes>
+            </main>
+          </div>
+        } />
+      </Routes>
     </Router>
   );
 }
